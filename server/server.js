93b588b44524ff50608e5721859be789f26e179a
Vinh Le <vltechno@outlook.com>
2018-02-09 23:41:18 +0700

    shit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 require('./config/config.js');
 const path = require('path');
 const express = require('express');
-
-var app = express();
+const socketIO = require('socket.io');
 const port = process.env.PORT;
 const publicPath = path.join(__dirname,'../public');
+const http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath)); // middle ware
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('disconnect from client');
+  });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
